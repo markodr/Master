3a8255d2ff2b6ca4b835aa6122ecbab4e15b3f85
Marko <mdragoslavic@supplyframe.com>
2016-07-18 22:15:21 +0200

    C aktivira kalibraciju, sve radi u negativnoj logici, podesiti LabView

---
+++ Keil/main.cpp
@@ -0,0 +1,136 @@
+#include "mbed.h"
+#include <stdlib.h> 
+
+#define pauza 0.01
+#define prag 8
+Serial pc(USBTX, USBRX); // tx, rx
+
+// Na ovm piovima se nalaze senzori
+AnalogIn   Senzor0(A0);
+AnalogIn   Senzor1(A1);
+AnalogIn   Senzor2(A2);
+AnalogIn   Senzor3(A3);
+
+// Promenljive
+float senzori[4];
+char prijem[5];
+float threshold;
+
+// Sve se salje kao float string 8 bajta odnostno 6 cifara iza nule !!
+
+float recive_data(){
+// poslati dva puta da se aktivira Data Ready
+
+	float treshold[4];
+	int i;
+	float minimum;
+	char prijem[5]; // Malo veci buffer za prijem ne koristi se ceo
+	for (i=0;i<=1;i++){
+		prijem[i]=pc.getc();	
+	}
+
+// Jedan bafer nije na 0, ocistiti ga
+	if (prijem[0]=='C'){
+			for(i=1;i<=10;i++){
+	treshold[0]=treshold[0]+Senzor0.read();
+	treshold[1]=treshold[1]+Senzor1.read();
+	treshold[2]=treshold[2]+Senzor2.read();
+	treshold[3]=treshold[3]+Senzor3.read();	
+	}
+			treshold[0]=treshold[0]/10;
+			treshold[1]=treshold[1]/10;
+			treshold[2]=treshold[2]/10;
+			treshold[3]=treshold[3]/10;	
+			minimum=treshold[0]; // Pocni od 1 jer je deifinisan maximum
+			for (i=1;i<=3;i++){
+				if (treshold[i]<minimum){
+					minimum=treshold[i];
+				}
+			}
+			return minimum;
+	}
+	else {
+		minimum=0;
+		return minimum;
+	}
+}    
+void send_data(){
+		 int i;
+     senzori[0]=Senzor0.read();
+     wait(pauza);
+     senzori[1]=Senzor1.read();
+     wait(pauza);
+     senzori[2]=Senzor2.read();
+     wait(pauza);
+     senzori[3]=Senzor3.read();
+     wait(pauza);
+     for(i=0;i<=3;i++){
+				pc.printf("%f\n",senzori[i]);
+      }
+     pc.printf("END  END\n");
+}
+main() {
+    
+  pc.printf("Hello World !\n");
+  while(1) { 
+// Salji podatke sa senzora
+		send_data();
+// Proveri da li je stigao zahtev za treshold
+		if( pc.readable()){ 
+		threshold=recive_data();
+		pc.printf("\nThreshold =%f \n",threshold);
+		}
+			
+
+  }// While
+} // Main
+
+ 
+
+    
+    
+    
+
+    
+/*
+while(1){
+        
+        if(Senzor1.read()<=0.5){
+            pc.printf("1.000000\n");
+            }
+        else{
+            pc.printf("0.000000\n");
+            }    
+        wait(pauza);
+        
+        if(Senzor2.read()<=0.5){
+            pc.printf("1.000000\n");
+            }
+        else{
+            pc.printf("0.000000\n");
+            }    
+        wait(pauza);
+        
+        if(Senzor3.read()<=0.5){
+            pc.printf("1.000000\n");
+            }
+        else{
+            pc.printf("0.000000\n");
+            }    
+        wait(pauza);
+        if(Senzor4.read()<=0.5){
+            pc.printf("1.000000\n");
+            }
+        else{
+            pc.printf("0.000000\n");
+            }    
+        wait(pauza);        
+
+        
+        
+        pc.printf("END  END\n");
+        wait(pauza);
+        }
+}
+*/
+        
