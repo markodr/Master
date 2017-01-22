c7298ce4d1b403267d8a54fc140720cd421aab2a
Marko <mdragoslavic@supplyframe.com>
2017-01-22 23:07:30 +0100

    Test 2xACC

--- Ose_ACC_1.m
+++ Ose_ACC_1.m
@@ -12,33 +12,30 @@ plot(ACC2_X);
 %%
 clc; close all; clear all;
 pauza=(0.3);
-x=0; y=0; z=0; tr=1; score=0; i=0; iter=10;
-xi=[0,0]; yi=[0,0]; zi=[0,0];
+x=0; y=0; z=0; tr=1; score=0; i=0; iter=10; control='STOP'
+xs=[];ys=[];zs=[];
+
+% Treba mi brojaca koji se resetuje sa STOP
 
 sekvenca=[1,2,3,1,8];
 % Dinamicki kreiram sekvencu
 prijem = zeros(1,length(sekvenca));
 
+
 % Testiram X
 if x>=tr && x>=y && x>=z % Aktiviran je ACC
-    xi(0)=iter;
-    xsad=1;
-
-    prijem(i)=1;
-    i=i+1;
-    wait(pauza);
+ 
+    
 end
 % Testiram Y
 if y>=tr && y>=x && y>=z
-    prijem(i)=2;
-    i=i+1;
-    wait(pauza);
+    yi(1)=y;
+    yi(2)=iter;
 end
 % Testiram Z
 if z>=tr && z>=x && z>=y
-    prijem(i)=2;
-    i=i+1;
-    wait(pauza);
+    zi(1)=z;
+    zi(2)=iter;
 end
 
 score=prijem;
