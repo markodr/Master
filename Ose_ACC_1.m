25b0f12eaa992aedaf93ef036438440b2b9aca01
Marko <mdragoslavic@supplyframe.com>
2017-01-24 08:01:31 +0100

    Akvizicija x3 ACC, export Excel, load Matlab

--- Ose_ACC_1.m
+++ Ose_ACC_1.m
@@ -1,42 +1,34 @@
 % Brisi pre rucnog importa
 clc;
 close all;
-clear all;
-% Import data
+%clear all;
+
+
+%% ACC 1
 figure();
 hold on;
-plot(ACC1_X);
-plot(ACC1_Y);
-plot(ACC2_X);
+plot(Bela1);
+plot(Zelena1);
+plot(Zuta1);
+hold off;
+%% ACC 2
+figure();
+hold on;
+plot(Bela2);
+plot(Zelena2);
+plot(Zuta2);
+hold off;
 
-%%
-clc; close all; clear all;
-pauza=(0.3);
-x=0; y=0; z=0; tr=1; score=0; i=0; iter=10; control='STOP'
-xs=[];ys=[];zs=[];
-
-% Treba mi brojaca koji se resetuje sa STOP
-
-sekvenca=[1,2,3,1,8];
-% Dinamicki kreiram sekvencu
-prijem = zeros(1,length(sekvenca));
+%% ACC 3
+figure();
+hold on;
+%plot(Bela3);
+plot(Zelena3);
+plot(Zuta3);
+hold off;
 
 
-% Testiram X
-if x>=tr && x>=y && x>=z % Aktiviran je ACC
- 
-    
-end
-% Testiram Y
-if y>=tr && y>=x && y>=z
-    yi(1)=y;
-    yi(2)=iter;
-end
-% Testiram Z
-if z>=tr && z>=x && z>=y
-    zi(1)=z;
-    zi(2)=iter;
-end
 
-score=prijem;
-clear prijem
+
+
+
