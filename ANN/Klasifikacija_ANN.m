c9d46f55f7f8492c7e8f875a8385e088e9096886
Marko <mdragoslavic@supplyframe.com>
2017-09-18 19:21:55 +0200

    Pred test

--- ANN/Klasifikacija_ANN.m
+++ ANN/Klasifikacija_ANN.m
@@ -1,13 +1,20 @@
+% 8 IzlazMiruje =  [0;0;0;1];
+% 4 IzlazDole =    [0;0;1;0]; 
+% 2 IzlazSredina = [0;1;0;0]; 
+% 1 IzlazGore =    [1;0;0;0];
+
 clc; clear all; close all;
 load ANN.mat;
 load Signali.mat;
-
-Ulaz=[GORE,SREDINA,DOLE];
+Ulaz=[GORE,SREDINA,DOLE,MIRUJE];
 Izlaz = net(Ulaz);
-
+%
 Round_Izlaz=round(Izlaz);
 Decimalini_Izlaz=[];
 for i=1:length(Round_Izlaz)
     Decimalini_Izlaz= [Decimalini_Izlaz ; bi2de(Round_Izlaz(:,i)')];
 end
-Decimalini_Izlaz=Decimalini_Izlaz';
+'Gore 1, Sredina 2, Dole 4, Miruje 8'
+Decimalini_Izlaz=Decimalini_Izlaz'
+
+% OCEKUJEM 1 2 4 8
--- ANN/OBUKA_ANN.m
+++ ANN/OBUKA_ANN.m
@@ -7,36 +7,40 @@ Broj_semplova=8;
 [Dole1,Dole2,Dole3] = importfile('DOLEPeakToPeak.lvm',1, Broj_semplova);
 [Sredina1,Sredina2,Sredina3] = importfile('SREDINAPeakToPeak.lvm',1, Broj_semplova);
 [Gore1,Gore2,Gore3] = importfile('GOREPeakToPeak.lvm',1, Broj_semplova);
+[Miruje1,Miruje2,Miruje3] = importfile('MIRUJEPeakToPeak.lvm',1, Broj_semplova);
 
 % Kreiranje ulaznog vektora
 GORE=[Gore1';Gore2';Gore3'];
 SREDINA=[Sredina1';Sredina2';Sredina3'];
 DOLE=[Dole1';Dole2';Dole3'];
+MIRUJE=[Miruje1';Miruje2';Miruje3'];
 
 %Kreiranje Izaznog Vektora
-IzlazDole=[0;0;1]; 
-IzlazSredina=[0;1;0]; 
-IzlazGore=[1;0;0];
+IzlazMiruje =  [0;0;0;1];
+IzlazDole =    [0;0;1;0]; 
+IzlazSredina = [0;1;0;0]; 
+IzlazGore =    [1;0;0;0];
 
-DOLE_IZLAZ=[];
-SREDINA_IZLAZ=[];
-GORE_IZLAZ =[];
+DOLE_IZLAZ = [];
+SREDINA_IZLAZ = [];
+GORE_IZLAZ = [];
+MIRUJE_IZLAZ = [];
 
 for i=1:Broj_semplova
     DOLE_IZLAZ = [DOLE_IZLAZ,IzlazDole];
     SREDINA_IZLAZ = [SREDINA_IZLAZ,IzlazSredina];
     GORE_IZLAZ = [GORE_IZLAZ,IzlazGore];
+    MIRUJE_IZLAZ =[MIRUJE_IZLAZ,IzlazMiruje];
 end
 
 % Obucavanje ANN
-Ulaz=[GORE,SREDINA,DOLE];
-Izlaz=[GORE_IZLAZ,SREDINA_IZLAZ,DOLE_IZLAZ];
-
+Ulaz=[MIRUJE,GORE,SREDINA,DOLE];
+Izlaz=[MIRUJE_IZLAZ,GORE_IZLAZ,SREDINA_IZLAZ,DOLE_IZLAZ];
 
 net = patternnet(10);
 [net,tr] = train(net,Ulaz,Izlaz);
 nntraintool;
 
-save('ANN.mat','net')
+save('ANN.mat','net');
 
-save('Signali.mat','GORE','SREDINA','DOLE');
+save('Signali.mat','MIRUJE','GORE','SREDINA','DOLE');
